ade6959a11ae7bea29d5955982f8f30f41b40cae
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2026-02-10 23:39:15 +0530

    Made changes to the test file to include tests for Gameboard class function, receiveAttack(), and placeShip(), might need to change these tests later, as the development progresses.

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "./script";
+import { Ship, Gameboard } from "./script";
 
 test("Ship: hitFunction check", () => {
 	const shipObj = new Ship(3, "(E,4)", "V");
@@ -22,4 +22,52 @@ test("Ship: isSunk check - True", () => {
 	shipObj.hit();
 	shipObj.isSunk();
 	expect(shipObj.sunkValue).toBeTruthy();
+});
+
+test("Gameboard: receiveAttack - right co-ordinate", () => {
+	const gameBoard = new Gameboard();
+	expect(gameBoard.receiveAttack("(A, 3)")).toBeTruthy();
+});
+
+test("Gameboard: receiveAttack - incorrect co-ordinate (x-axis)", () => {
+	const gameBoard = new Gameboard();
+	expect(gameBoard.receiveAttack("(T, 3)")).toBeFalsy();
+});
+
+test("Gameboard: receiveAttack - incorrect co-ordinate (y-axis)", () => {
+	const gameBoard = new Gameboard();
+	expect(gameBoard.receiveAttack("(D, 12)")).toBeFalsy();
+});
+
+test("Gameboard: receiveAttack - same co-ordinate", () => {
+	const gameBoard = new Gameboard();
+	gameBoard.receiveAttack("B, 2");
+	expect(gameBoard.receiveAttack("B, 2")).toBeFalsy();
+});
+
+test("Gameboard: placeShip - right co-ordinate", () => {
+	const gameBoard = new Gameboard();
+	expect(gameBoard.placeShip(new Ship(3, "(E,4)", "V"))).toBeTruthy();
+});
+
+test("Gameboard: placeShip - incorrect co-ordinate (x-axis)", () => {
+	const gameBoard = new Gameboard();
+	expect(gameBoard.placeShip(new Ship(3, "(Y,4)", "V"))).toBeFalsy();
+});
+
+test("Gameboard: placeShip - incorrect co-ordinate (y-axis)", () => {
+	const gameBoard = new Gameboard();
+	expect(gameBoard.placeShip(new Ship(3, "(E,-1)", "V"))).toBeFalsy();
+});
+
+test("Gameboard: placeShip - same co-ordinate", () => {
+	const gameBoard = new Gameboard();
+	gameBoard.placeShip(new Ship(3, "(A, 7)", "V"));
+	expect(gameBoard.placeShip(new Ship(3, "(A, 7)", "V"))).toBeFalsy();
+});
+
+test("Gameboard: placeShip - overlap check", () => {
+	const gameBoard = new Gameboard();
+	gameBoard.placeShip(new Ship(3, "(A, 7)", "V"));
+	expect(gameBoard.placeShip(new Ship(3, "(B, 7)", "V"))).toBeFalsy();
 });
