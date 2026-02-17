e83bac176205e6117827dbb117cb04dc9fd9330f
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2026-02-17 23:55:06 +0530

    Changed the co-ordinate naming convention, to match how it is in the real game.

--- script.test.js
+++ script.test.js
@@ -1,14 +1,14 @@
 import { Ship, Gameboard } from "./script";
 
 test("Ship: hitFunction check", () => {
-	const shipObj = new Ship(3, "(E,4)", "V");
+	const shipObj = new Ship(3, "E-4", "V");
 	shipObj.hit();
 	shipObj.hit();
 	expect(shipObj.numberOfHits).toBe(2);
 });
 
 test("Ship: isSunk check - False", () => {
-	const shipObj = new Ship(3, "(E,4)", "V");
+	const shipObj = new Ship(3, "E-4", "V");
 	shipObj.hit();
 	shipObj.hit();
 	shipObj.isSunk();
@@ -16,7 +16,7 @@ test("Ship: isSunk check - False", () => {
 });
 
 test("Ship: isSunk check - True", () => {
-	const shipObj = new Ship(3, "(E,4)", "V");
+	const shipObj = new Ship(3, "E-4", "V");
 	shipObj.hit();
 	shipObj.hit();
 	shipObj.hit();
@@ -26,48 +26,48 @@ test("Ship: isSunk check - True", () => {
 
 test("Gameboard: receiveAttack - right co-ordinate", () => {
 	const gameBoard = new Gameboard();
-	expect(gameBoard.receiveAttack("(A, 3)")).toBeTruthy();
+	expect(gameBoard.receiveAttack("A-3")).toBeTruthy();
 });
 
 test("Gameboard: receiveAttack - incorrect co-ordinate (x-axis)", () => {
 	const gameBoard = new Gameboard();
-	expect(gameBoard.receiveAttack("(T, 3)")).toBeFalsy();
+	expect(gameBoard.receiveAttack("T-3")).toBeFalsy();
 });
 
 test("Gameboard: receiveAttack - incorrect co-ordinate (y-axis)", () => {
 	const gameBoard = new Gameboard();
-	expect(gameBoard.receiveAttack("(D, 12)")).toBeFalsy();
+	expect(gameBoard.receiveAttack("D-12")).toBeFalsy();
 });
 
 test("Gameboard: receiveAttack - same co-ordinate", () => {
 	const gameBoard = new Gameboard();
-	gameBoard.receiveAttack("B, 2");
-	expect(gameBoard.receiveAttack("B, 2")).toBeFalsy();
+	gameBoard.receiveAttack("B-2");
+	expect(gameBoard.receiveAttack("B-2")).toBeFalsy();
 });
 
 test("Gameboard: placeShip - right co-ordinate", () => {
 	const gameBoard = new Gameboard();
-	expect(gameBoard.placeShip(new Ship(3, "(E,4)", "V"))).toBeTruthy();
+	expect(gameBoard.placeShip(new Ship(3, "E-4", "V"))).toBeTruthy();
 });
 
 test("Gameboard: placeShip - incorrect co-ordinate (x-axis)", () => {
 	const gameBoard = new Gameboard();
-	expect(gameBoard.placeShip(new Ship(3, "(Y,4)", "V"))).toBeFalsy();
+	expect(gameBoard.placeShip(new Ship(3, "Y-4", "V"))).toBeFalsy();
 });
 
 test("Gameboard: placeShip - incorrect co-ordinate (y-axis)", () => {
 	const gameBoard = new Gameboard();
-	expect(gameBoard.placeShip(new Ship(3, "(E,-1)", "V"))).toBeFalsy();
+	expect(gameBoard.placeShip(new Ship(3, "E--1", "V"))).toBeFalsy();
 });
 
 test("Gameboard: placeShip - same co-ordinate", () => {
 	const gameBoard = new Gameboard();
-	gameBoard.placeShip(new Ship(3, "(A, 7)", "V"));
-	expect(gameBoard.placeShip(new Ship(3, "(A, 7)", "V"))).toBeFalsy();
+	gameBoard.placeShip(new Ship(3, "A-7", "V"));
+	expect(gameBoard.placeShip(new Ship(3, "A-7", "V"))).toBeFalsy();
 });
 
 test("Gameboard: placeShip - overlap check", () => {
 	const gameBoard = new Gameboard();
-	gameBoard.placeShip(new Ship(3, "(A, 7)", "V"));
-	expect(gameBoard.placeShip(new Ship(3, "(B, 7)", "V"))).toBeFalsy();
+	gameBoard.placeShip(new Ship(3, "A-7", "V"));
+	expect(gameBoard.placeShip(new Ship(3, "B-7", "V"))).toBeFalsy();
 });
